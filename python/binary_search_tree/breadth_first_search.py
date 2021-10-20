0a66172e14fcb2ba54914810b9966f1d960ff782
pauloestrella <pauloestrella1994@gmail.com>
2021-10-20 16:56:06 -0300

    Breadth first search

---
+++ python/binary_search_tree/breadth_first_search.py
@@ -0,0 +1,41 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        queue = []
+        if root is None:
+            return
+        queue.append(root)
+        while len(queue) != 0:
+            node = queue.pop(0)
+            print(node.data, end =' ')
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+
+
+
+T=int(input("Number of elements in tree: "))
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input("Element to be inserted in tree: "))
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
